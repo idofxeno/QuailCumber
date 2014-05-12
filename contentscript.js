c8daf91cb8b03949a5227cb3ff09fb44e48120b7
Justin Brooks <jdb507@snagajob.com>
2014-05-12 11:25:34 -0400

    updated content script to include check box and radio button, reformatted record step to match home brewed capybara steps

--- contentscript.js
+++ contentscript.js
@@ -2,45 +2,49 @@
 //I am used here to attach an event to any click that will record steps and send them to the background page where the popup can retrieve them.
 
 function record(event){
-	//How will selenium select the element, find out if the element we clicked on has a selector (checks for id, name or class)
-	var elementSelector = '';
-	if(event.target.id){
-		elementSelector = 'id of "' + event.target.id + '"';
-	}else if(event.target.getAttribute('name')){
-		elementSelector = 'name of "' + event.target.getAttribute('name') + '"';
-	}else if(event.target.getAttribute('class')){
-		elementSelector = 'class of "' + event.target.getAttribute('class') + '"';
-	}
-	
-	//If we have a valid selector, lets record a step!
-	if(elementSelector.length > 0){
-	
-		console.log(event.target.type);
-		switch(event.target.type) {
-			//If the element we clicked on is an input, lets attach a onblur event to capture what was put into the field (attachs to password or text fields) TODO: Text Area
-			case 'text':
-			case 'password':
-				event.target.onblur = function(event){
-					if(event.target.value.length > 0){
-						chrome.extension.sendRequest({method: 'recordStep', data: 'Then I enter "' + event.target.value + '" into field with ' + elementSelector});
-					}
-				}
-				break;
-			//If the elemnt is a select, lets figure that out (select-one)
-			case 'select-one':
-				event.target.onchange = function(event){
-					if(true){//if(event.target.value.length > 0){
-						chrome.extension.sendRequest({method: 'recordStep', data: 'Then I select "' + event.target.value + '" from select with ' + elementSelector})
-					}
-				}
-				break;
-			//If the elemnt is a checkbox, lets figure that out
-			//If the elemnt is a select, lets figure that out
-			//If it is some other type of element, lets just poke it and hope for the best!
-			default:
-				var clickAction = 'Then I click on element ' + elementSelector + ' ' + event.target.type
-				chrome.extension.sendRequest({method: "recordStep", data: clickAction});
-		}
+	//Find out what type of element was clicked on and act accordingly
+	//console.log(event.target.type);
+	switch(event.target.type) {
+		//If the element we clicked on is an input, lets attach a onblur event to capture what was put into the field (attachs to password or text fields) TODO: Text Area
+		case 'text':
+		case 'password':
+			event.target.onblur = function(event){
+				var theStep = 'Then I enter "' + event.target.value + '" into "' + event.target.id + '"';
+				chrome.extension.sendRequest({method: 'recordStep', data: theStep});
+				event.target.onblur = null;
+			}
+			break;
+		//If the element is a select, lets figure that out (select-one)
+		case 'select-one':
+			event.target.onchange = function(event){
+				var theStep = 'Then I select "' + event.target.value + '" from "' + event.target.id + '"';
+				chrome.extension.sendRequest({method: 'recordStep', data: theStep})
+				event.target.onchange = null;
+			}
+			break;
+		//If the elemnt is a checkbox, check it by value
+		case 'checkbox':
+			var theStep = 'I check the box with value "' + event.target.value + '"';
+			chrome.extension.sendRequest({method: "recordStep", data: theStep});
+			break;
+		//If the elemnt is a select, choose by id
+		case 'radio':
+			var theStep = 'Then I choose "' + event.target.id + '"';
+			chrome.extension.sendRequest({method: 'recordStep', data: theStep});
+			break;
+		//If it is some other type of element, lets just poke it and hope for the best!
+		default:
+			var theStep = '';
+			if(event.target.id){
+				theStep = 'Then I click on element with id "' + event.target.id + '"';
+			}else if(event.target.getAttribute('name')){
+				theStep = 'Then I click on a "' + event.target.TAGNAME +'" element with name "' + event.target.getAttribute('name') + '"';
+			}else if(event.target.getAttribute('class')){
+				theStep = 'Then I click on element with class "' + event.target.getAttribute('class') + '"';
+			}else{
+				theStep = 'WARNING:: No valid selector found'
+			}
+			chrome.extension.sendRequest({method: "recordStep", data: theStep});
 	}
 }
 
